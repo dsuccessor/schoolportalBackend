9706e204bb4bc8f311b5e6088ddd4489c4daab67
Salaudeen Kehinde <salaudeenkr@gmail.com>
2023-06-29 08:29:04 +0100

    updating .env content

--- helper/imageUpload.js
+++ helper/imageUpload.js
@@ -4,12 +4,9 @@ const cloudinary = require("cloudinary").v2;
 // Return "https" URLs by setting secure: true
 cloudinary.config({
   secure: true,
-  //cloud_name: process.env.CLOUDINARY_USER_NAME,
-  cloud_name: "schoolportal",
-  //api_key: process.env.CLOUDINARY_API_KEY,
-  api_key: "197462131525526",
-  //api_secret: process.env.CLOUDINARY_API_SECRET,
-  api_secret: "DPO8PipjkotPrG_cO60ZhjDLK_4",
+  cloud_name: process.env.CLOUDINARY_USER_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 // Log the configuration
