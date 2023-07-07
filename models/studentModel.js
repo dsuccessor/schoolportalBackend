9fbfa44e34a1d0276b49f3511d58f8dbcc50764b
Salaudeen Kehinde <salaudeenkr@gmail.com>
2023-07-07 19:45:40 +0100

    updating studentregisteration api

--- models/studentModel.js
+++ models/studentModel.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose')
-const autoIncrement = require('mongoose-auto-increment')
-autoIncrement.initialize(mongoose.connection)
+const mongoose = require("mongoose");
+const autoIncrement = require("mongoose-auto-increment");
+autoIncrement.initialize(mongoose.connection);
 
 const studentSchema = new mongoose.Schema(
   {
@@ -23,7 +23,7 @@ const studentSchema = new mongoose.Schema(
       type: String,
       required: true,
       lowercase: true,
-      enum: ['male', 'female'],
+      enum: ["male", "female"],
     },
     dob: {
       type: Date,
@@ -75,21 +75,117 @@ const studentSchema = new mongoose.Schema(
   },
   {
     timestamps: true,
-  },
-)
+  }
+);
 
 studentSchema.plugin(autoIncrement.plugin, {
-  model: 'student',
-  field: 'studentId',
+  model: "student",
+  field: "studentId",
   startAt: 10001,
   incrementBy: 395,
-})
+});
 studentSchema.plugin(autoIncrement.plugin, {
-  model: 'student',
-  field: 'matricNo',
+  model: "student",
+  field: "matricNo",
   startAt: 20220001,
   incrementBy: 1,
-})
-const studentModel = mongoose.model('student', studentSchema)
+});
+const studentModel = mongoose.model("student", studentSchema);
 
-module.exports = studentModel
+module.exports = studentModel;
+
+// const mongoose = require('mongoose')
+// const autoIncrement = require('mongoose-auto-increment')
+// autoIncrement.initialize(mongoose.connection)
+
+// const studentSchema = new mongoose.Schema(
+//   {
+//     studentId: {
+//       type: Number,
+//       required: true,
+//       unique: true,
+//     },
+//     surname: {
+//       type: String,
+//       required: true,
+//       lowercase: true,
+//     },
+//     otherName: {
+//       type: String,
+//       required: true,
+//       lowercase: true,
+//     },
+//     gender: {
+//       type: String,
+//       required: true,
+//       lowercase: true,
+//       enum: ['male', 'female'],
+//     },
+//     dob: {
+//       type: Date,
+//       required: true,
+//     },
+//     email: {
+//       type: String,
+//       required: true,
+//       lowercase: true,
+//       unique: true,
+//       regex: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+//     },
+//     matricNo: {
+//       type: Number,
+//       required: true,
+//       unique: true,
+//     },
+//     phoneNo: {
+//       type: Number,
+//       required: true,
+//       unique: true,
+//     },
+//     passport: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//     },
+//     password: {
+//       type: String,
+//       required: true,
+//       minlength: 8,
+//       maxlength: 64,
+//     },
+//     level: {
+//       type: String,
+//       required: true,
+//       lowercase: true,
+//     },
+//     faculty: {
+//       type: String,
+//       required: true,
+//       lowercase: true,
+//     },
+//     department: {
+//       type: String,
+//       required: true,
+//       lowercase: true,
+//     },
+//   },
+//   {
+//     timestamps: true,
+//   },
+// )
+
+// studentSchema.plugin(autoIncrement.plugin, {
+//   model: 'student',
+//   field: 'studentId',
+//   startAt: 10001,
+//   incrementBy: 395,
+// })
+// studentSchema.plugin(autoIncrement.plugin, {
+//   model: 'student',
+//   field: 'matricNo',
+//   startAt: 20220001,
+//   incrementBy: 1,
+// })
+// const studentModel = mongoose.model('student', studentSchema)
+
+// module.exports = studentModel
--- routes/studentRoute.js
+++ routes/studentRoute.js
@@ -1,12 +1,42 @@
-const studentControl = require('../controls/studentControl')
-const express = require('express')
-const router = express.Router()
+const studentControl = require("../controls/studentControl");
+const multer = require("multer");
+const express = require("express");
+const router = express.Router();
 
-router.post('/register', studentControl.createStudent)
-router.get('/fetchAll', studentControl.fetchAllStudent)
-router.get('/fetch/:email', studentControl.fetchStudent)
-router.put('/update/:email', studentControl.updateStudent)
-router.put('/updatebyid/:id', studentControl.updateById)
-router.delete('/delete/:id', studentControl.delStudent)
+const storage = multer.diskStorage({});
 
-module.exports = router
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    cb("Wrong file format, Make sure you are uploading an image file!", false);
+  }
+};
+
+const uploads = multer({ storage, fileFilter });
+
+router.post(
+  "/register",
+  uploads.single("passport"),
+  studentControl.createStudent
+);
+router.get("/fetchAll", studentControl.fetchAllStudent);
+router.get("/fetch/:email", studentControl.fetchStudent);
+router.put("/update/:email", studentControl.updateStudent);
+router.put("/updatebyid/:id", studentControl.updateById);
+router.delete("/delete/:id", studentControl.delStudent);
+
+module.exports = router;
+
+// const studentControl = require('../controls/studentControl')
+// const express = require('express')
+// const router = express.Router()
+
+// router.post('/register', studentControl.createStudent)
+// router.get('/fetchAll', studentControl.fetchAllStudent)
+// router.get('/fetch/:email', studentControl.fetchStudent)
+// router.put('/update/:email', studentControl.updateStudent)
+// router.put('/updatebyid/:id', studentControl.updateById)
+// router.delete('/delete/:id', studentControl.delStudent)
+
+// module.exports = router
